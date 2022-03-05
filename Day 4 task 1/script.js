e27c93072309eaa41c09b2d4701d8b01ad6aec2e
Vasanthakumar <99638420+Vasanthphr@users.noreply.github.com>
2022-03-05 10:39:42 +0530

    Add files via upload

---
+++ Day 4 task 1/script.js
@@ -0,0 +1,17 @@
+
+// Task-  Use the rest countries API url -> https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json and display all the country flags in console
+
+
+var  request =new XMLHttpRequest();
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+request.send();
+request.onload=function(){
+    var data=JSON.parse(request.response);
+    console.log(data);
+
+
+for (var i=0;i<data.length;i++){
+    console.log(`flag:${data[i].flag}`);
+   };
+
+}
